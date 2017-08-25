03b5b47f47fedf3554f4328fea5c00322a6f1bd9
Isabelalopes <isabelamlopes.93@gmail.com>
2017-08-25 14:30:37 -0300

    Delete selectedType.js

--- projetoSICOOB1907/projetoSICOOB/sicoobProject/keepcredufu/angular/selectedType.js
+++
@@ -1,7 +0,0 @@
-var selectTipos = document.getElementById('fileType');
-var divRetificada = document.querySelector('div[data-name="retificada"]');
-
-selectTipos.addEventListener('change', function () {
-    var escolhido = this.options[this.selectedIndex].dataset.section;
-    divRetificada.classList[escolhido == 'retificada' ? 'add' : 'remove']('mostrar');
-});
